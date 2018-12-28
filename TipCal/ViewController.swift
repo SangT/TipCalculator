cd2e1e6eed0034b1f84a943d17dde645fe2c8c73
Sang To <graceto@Sangs-MacBook-Air.local>
2018-12-27 20:37:18 -0800

    Initial Commit

---
+++ TipCal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCal
+//
+//  Created by Sang To on 12/27/18.
+//  Copyright © 2018 Grace To. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
