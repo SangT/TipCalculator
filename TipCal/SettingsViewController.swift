5125ec496038db99a3bfaa48b29479320f938eda
Sang To <tps.sang@gmail.com>
2018-12-29 18:10:17 -0800

    Removed unnecessary functions

--- TipCal/SettingsViewController.swift
+++ TipCal/SettingsViewController.swift
@@ -19,28 +19,16 @@ class SettingsViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("view will appear")
         let defaults = UserDefaults.standard
         tipControlSet.setEnabled(true, forSegmentAt: defaults.integer(forKey: "myInt"))
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("view did appear")
-    }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("view will disappear")
         let defaults = UserDefaults.standard
         let position = tipControlSet.selectedSegmentIndex
         defaults.set(position, forKey: "myInt")
         defaults.synchronize()
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("view did disappear")
     }
 
     /*
